1119365e3d7fbb8e8ecacd09220aebc3c1de38af
hunglk2011 <hle47285@gmail.com>
2024-11-16 02:10:12 +0700

    fix error syntax

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,10 +4,18 @@ type Params = {
   };
 };
 
+// `generateMetadata` does not need to return a promise
 export async function generateMetadata({ params }: Params) {
   return { title: `Post: ${params.slug}` };
 }
 
+// The Page component expects the `params` prop to be passed
 export default function Page({ params }: Params) {
-  return <><h1>Slug: {params.slug}</h1> <p>HCMUE</p> <p>2024</p></>;
+  return (
+    <>
+      <h1>Slug: {params.slug}</h1>
+      <p>HCMUE</p>
+      <p>2024</p>
+    </>
+  );
 }
